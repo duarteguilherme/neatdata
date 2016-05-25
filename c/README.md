e5ec4de2640171990143975c1befb0d95777e61c
gjardimduarte@gmail.com <dunha@dunha.com>
2016-05-25 11:24:29 -0300

    C version

---
+++ c/README.md
@@ -0,0 +1,3 @@
+# C Version for neatdata
+
+I hope this can end well
---
+++ c/unnest.c
@@ -0,0 +1,14 @@
+/*  |
+ *  | neatdata - wrangling your data on terminal 
+ *  |
+ *  | unnest 
+ *  |
+ *  |
+ */
+
+#include <stdio.h>
+#include <libcsv.h>
+int main() {
+
+    return 0;
+}
